f4aaecfccc7475cc2602e25cbbc1c5b444edefb7
SarabjitSingh18 <166316546+SarabjitSingh18@users.noreply.github.com>
2025-08-30 23:09:22 +0530

    bug fixed

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -3,7 +3,7 @@ import { ENV } from "./env.js";
 export const connectDB = async()=>{
     try {
         if(!ENV.MONGO_URI){
-            throw new Error("MONGO_URI is not defined please check env!!!")
+            throw new Error("MONGO_URI is not defined please check env variables!!!")
         }
         const conn = await mongoose.connect(ENV.MONGO_URI)
         console.log(`MongoDB Connected: ${conn.connection.host}`)
